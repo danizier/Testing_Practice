b04ffd3d7c713fed32bdfe7ceb99d6e7a778ad59
Daniel Zierer <daniel.zierer@draexlmaier.com>
2024-01-10 10:14:06 +0100

    Wrote all test scenarios

---
+++ script_test.js
@@ -0,0 +1,23 @@
+const { capitalize, reverseString, calculator, caesarCipher, analyzeArray} = require('./script')
+
+test('First character capitalized')
+
+test('String is reversed')
+
+test('adding two numbers')
+
+test('subtracting two numbers')
+
+test('divide two numbers')
+
+test('multiply two numbers')
+
+test('String Character are shifted')
+
+test('Average Array Value')
+
+test('Min Array Value')
+
+test('Max Array Value')
+
+test('Array Length')
